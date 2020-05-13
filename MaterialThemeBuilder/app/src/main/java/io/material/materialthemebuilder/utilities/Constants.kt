1beec122f2ffb19e406fdfedefa8be4f7c7ba43b
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-05-13 21:00:18 +0200

    added scp id constants

---
+++ MaterialThemeBuilder/app/src/main/java/io/material/materialthemebuilder/utilities/Constants.kt
@@ -0,0 +1,6 @@
+package io.material.materialthemebuilder.utilities
+
+// keep updated with mode enum in [attrs.xml]
+const val SCP_VIEW_MODE_CONTAINMENT = 0
+const val SCP_VIEW_MODE_DISRUPTION = 1
+const val SCP_VIEW_MODE_RISK = 2
